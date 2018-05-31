c262bea808f5f07e520db09a4a4df6c92b644fb9
F.S. Nobels <fonotec@hotmail.com>
2018-05-31 11:23:29 +0200

    Add data set combining feature

--- high.py
+++ high.py
@@ -5,6 +5,17 @@ import os
 # HI Galaxy Hydrogen detector
 # HIGH
 
+def printFiles(direct):
+    lsarray = os.listdir(direct)
+    print('Choose an on source and off source file to calculate the spectrum:')
+    for i in range(len(lsarray)):
+        print(lsarray[i])
+
+def askInputFiles():
+    file1 = str(input('Name of file on source: '))
+    file2 = str(input('Name of file off source: '))
+    return file1, file2
+
 live = False 
 obstime = 1/60
 if live:
@@ -54,13 +65,9 @@ if live:
 else:
     print('The after processing pipline of HIGH is started')
     direct = './data/'
-    lsarray = os.listdir(direct)
-    print('Choose an on source and off source file to calculate the spectrum:')
-    for i in range(len(lsarray)):
-        print(lsarray[i])
+    printFiles(direct)
     
-    file1 = str(input('Name of file on source: '))
-    file2 = str(input('Name of file off source: '))
+    file1, file2 = askInputFiles()
     #file1 = 'X_CygA_L_000_u_+0.00_+0.00'
     #file2 = 'X_CygA_L_000_r_+1.00_+0.00'
 
@@ -86,7 +93,7 @@ else:
         print('1. I want to do a smoothing to the data (e.g. binning)!')
         print('2. I want to add more datasets of the same type!')
         print('3. I want to add a weighted average in the plot!')
-        print('4. Nevermind, I want to quit!')
+        print('0. Nevermind, I want to quit!')
         print('#'*60)
         while True: 
             option = int(input('Which option do you want to do? '))
@@ -95,29 +102,72 @@ else:
             else: 
                 print('Specify a valid input!')
         if option == 1:
-            smoothingfac = int(input('What is the smoothing factor?'))
+            smoothingfac = int(input('What is the smoothing factor? '))
             lensmooth = int(np.floor(len(onsource)/smoothingfac))
             #print(lensmooth)
             #print(np.zeros(lensmooth+1))
             onsourcesmooth = np.zeros(lensmooth+1)
             offsourcesmooth = np.zeros(lensmooth+1)
+            freqsmooth = np.zeros(lensmooth+1)
             #onsourcesmooth = np.zeros((lensmooth+1.))
             #offsourcesmooth = np.zeros((lensmooth+1.))
             
             for i in range(lensmooth):
                 onsourcesmooth[i] = np.sum(onsource[i*smoothingfac:(i+1)*smoothingfac])/smoothingfac
                 offsourcesmooth[i] = np.sum(offsource[i*smoothingfac:(i+1)*smoothingfac])/smoothingfac
+                freqsmooth[i] =np.sum(freq[i*smoothingfac:(i+1)*smoothingfac])/smoothingfac
             onsourcesmooth[-1]= np.sum(onsource[-1:-smoothingfac])/smoothingfac
-            offsourcesmooth[-1]= np.sum(offsource[-1:-smoothingfac])/smoothingfac
-            plt.plot(onsourcesmooth/offsourcesmooth)
+            offsourcesmooth[-1]= np.sum(offsource[-1:-smoothingfac])/smoothingfac   
+            freqsmooth[-1] = np.sum(freq[-1:-smoothingfac])/smoothingfac      
+            plt.plot(freqsmooth,onsourcesmooth/offsourcesmooth)
+            if noline!='Yes':
+                plt.axvline(x=1420.405,c='r')
             plt.show()
             
             
         elif option==2:
-            print('test')
+            printFiles(direct)
+            file1new, file2new = askInputFiles()
+            onsource2 = np.loadtxt(direct+file1new,usecols=1)
+            offsource2 = np.loadtxt(direct+file2new,usecols=1)
+            try: 
+                notequal = str(input('Is the integration time different? '))
+            except:
+                notequal = 'No' 
+            if notequal == 'No':
+                onsourcenew = (onsource+onsource2)/2
+                offsourcenew = (offsource+offsource2)/2
+            else:
+                weight1 = int(input('Total time current measurement? '))
+                weight2 = int(input('Total time new measurement? '))
+                onsourcenew = (onsource*weight1 + onsource2*weight2)/(weight1+weight2)
+                offsourcenew = (offsource*weight1 + offsource2*weight2)/(weight1+weight2)
+
+            plt.plot(freq,onsourcenew/offsourcenew,label='Combined Measurement')
+            plt.plot(freq,onsource/offsource,label='Old Measurement')
+            if noline!='Yes':
+                plt.axvline(x=1420.405,c='r')
+            plt.legend()
+            plt.show()
+            
+            try:
+                usenewset = str(input('Press enter if you want to keep this set, otherwise type NO.'))
+            except:
+                usenewset = True
+
+            if usenewset:
+                onsource = onsourcenew
+                offsource = offsourcenew
+            del onsourcenew
+            del offsourcenew
+            del onsource2
+            del offsource2
+
+            
+
         elif option==3:
             print('test')
-        elif option==4:
+        elif option==0:
             print('Thanks for using HIGH! :-)')
             exit(0)
 
